16040100f65ab6d9da0407fb0fc2fe9efcc0c8b0
IamHenrikS <138816201+IamHenrikS@users.noreply.github.com>
2025-12-22 19:56:16 +0100

    The dynamics of the cart-pole has been updated with RK4 in order to have a finer step for the controllers and the system has been update more accuratly in order to make sure that the system actually exhibit real behaviour.

--- dynamics/cartpole_dynamics.py
+++ dynamics/cartpole_dynamics.py
@@ -1,109 +1,160 @@
 """
-The equations of motion of the cartpole.
-Based on the followning derivation of the dynamics:
+Description: This class sets the enviornment (env) for the 
+cart pole problem. The following main structure is that the
+numerical integrator, Equations of Motion and state updates.
+
+The following sources has been used: 
 Source 1: Cart-Pole Optimal Control
 https://openmdao.github.io/dymos/examples/cart_pole/cart_pole.html
 Source 2: Cart-Pole System: Equations of motion.
 https://courses.ece.ucsb.edu/ECE594/594D_W10Byl/hw/cartpole_eom.pdf 
+Source 3: Main source for the EoM
+https://sharpneat.sourceforge.io/research/cart-pole/cart-pole-equations.html
 """
 import numpy as np
 
 class CartPoleDynamics:
-    def __init__(self, dt=0.02):
+    def __init__(self, dt=0.030):
+        """
+        Description: Initializes the parameters of the cart-pole. It also ensures
+        that the state is resetted().
+
+        Source: https://sharpneat.sourceforge.io/research/cart-pole/cart-pole-equations.html
+        """
         # Parameters:
         self.g = 9.82
-        self.m_cart = 1.0
-        self.m_pole = 0.1
-        self.m_total = self.m_cart + self.m_pole
-        self.l = 0.5
+        self.m_c = 1.0
+        self.m_p = 0.1
+        self.m_t = self.m_c + self.m_p
+        self.l = 1
         self.dt = dt
+        
+        # Friction
+        self.mu_c = 0.1  # Cart-track friction coeff
+        self.mu_p = 0.01 # Cart-pole friction coeff
 
-        # Dampning of the system
-        self.d_theta = 0.6
-        self.b_x = 0.1
+        # Limits (meters)
+        self.x_min = -5.5
+        self.x_max =  5.5
+
 
         # State
         self.reset()
 
     def reset(self):
         """
-        Docstring for reset
-        
-        :param self: self.theta is the angle of the
-        pole when it is hanging down vertically. The upright position 
-        would thus be pi (180 deg)
+        Description: State initialization.
+
+        Theta = 0: Upright, Theta < 0: Pole leaning left, Theta > 0: Pole leaning right.
         """
-        self.x = 0.0
-        self.x_dot = 0.0
-        self.theta = np.deg2rad(175)
-        self.theta_dot = 0.0
+        self.x = 0
+        self.x_dot = -1.0
+        self.theta = np.deg2rad(10)
+        self.theta_dot = np.deg2rad(0.0)
     
     @property
     def state(self):
+        """
+        Description: Creates new Numpy Array every call. 
+        """
         return np.array([self.x, self.x_dot, self.theta, self.theta_dot], dtype=float)
-    
-    
+ 
     def step(self, force):
-        g = self.g
-        m_p = self.m_pole
-        m_t = self.m_total
-        l = self.l
-        d_theta = self.d_theta
-        b_x = self.b_x
+        """
+        Description: Generates the steps itreative progressions of the dynamics.
+        Advance the cart-pole state by one timestep using 4th-order Runge-Kutta integration.
+        Improvements of the small steps in the system. This is based on the works of
+        https://sharpneat.sourceforge.io/research/cart-pole/cart-pole-equations.html where the
+        showcase of how RK4 and RK2 is more effective than Euler approximations of the dynamics.  
+        """
         dt = self.dt
 
-        x, x_dot, theta, theta_dot = self.x, self.x_dot, self.theta, self.theta_dot
+        def derivatives(state, force):
+            """
+            Description: Contains the derivative states of the dynamics
+            """
+            x, x_dot, theta, theta_dot = state
+            g, m_p, m_t, l, mu_p, mu_c = self.g, self.m_p, self.m_t, self.l, self.mu_p, self.mu_c
 
-        sin_t = np.sin(theta)
-        cos_t = np.cos(theta)
+            sin_t = np.sin(theta)
+            cos_t = np.cos(theta)
+            alpha = 4.0/3.0 - (m_p * cos_t**2)/m_t
 
-        # Angular acceleration (explicit form)
-        theta_acc = (
-            g * sin_t
-            - cos_t * (force + m_p * l * theta_dot**2 * sin_t) / m_t
-            - d_theta * theta_dot
-        ) / (
-            l * (4.0 / 3.0 - (m_p * cos_t**2) / m_t)
-        )
+            # Equations of Motion (ddot(theta), ddot(x))
+            # Lagrangian theory with non-conservative terms. 
+            theta_acc = (
+                g * sin_t 
+                - cos_t * (force + m_p * l * theta_dot**2 * sin_t) / m_t 
+                - (mu_p * theta_dot) / (l*alpha)
+            ) 
 
-        # Horizontal acceleration (explicit form)
-        x_acc = (
-            (force + m_p * l * theta_dot**2 * sin_t) / m_t
-            - (m_p * l * theta_acc * cos_t) / (m_t - b_x * x_dot)
-        )
+            x_acc = (
+                (force + m_p * l * theta_dot**2 * sin_t)/m_t 
+                - (m_p * l * theta_acc * cos_t / m_t)
+                - mu_c*x_dot
+            )
 
-        # Integrate
-        x += dt * x_dot
-        x_dot += dt * x_acc
-        theta += dt * theta_dot
-        theta_dot += dt * theta_acc
+            return np.array([x_dot, x_acc, theta_dot, theta_acc], dtype=float)
 
-        self.x = x
-        self.x_dot = x_dot
-        self.theta = theta
-        self.theta_dot = theta_dot
+        state = np.array([self.x, self.x_dot, self.theta, self.theta_dot], dtype=float)
 
+        k1 = derivatives(state, force)
+        k2 = derivatives(state + dt/2 * k1, force)
+        k3 = derivatives(state + dt/2 * k2, force)
+        k4 = derivatives(state + dt * k3, force)
 
-    def poleposition(self):
+        # RK4 and defining next state.
+        state_next = state + dt/6 * (k1 + 2*k2 + 2*k3 + k4)
+        self.x, self.x_dot, self.theta, self.theta_dot = state_next
+
+        # Defining the limits for the hardstop
+        if self.x < self.x_min:
+            self.x = self.x_min
+            if self.x_dot < 0:
+                self.x_dot = 0.0
+        elif self.x > self.x_max:
+            self.x = self.x_max
+            if self.x_dot > 0:
+                self.x_dot = 0.0
+
+    def pole_tip_position(self):
         """
-        Docstring for poleposition
-        
-        :param self: Return the position (x_pole, y_pole)
-        for the pole to be used in plotting.
+        Returns the Cartesian position of the pole tip.
+
+        Coordinate frame:
+        - x: horizontal (right positive)
+        - y: vertical (up positive)
+        - theta = 0 corresponds to pole upright
+
+        Assumptions:
+        - self.l is the distance from pivot to pole center of mass
+        - Full pole length = 2 * self.l
         """
-        L = self.l*2
-        x_pole = self.x + L * np.sin(self.theta)
-        y_pole = -L * np.cos(self.theta)
-        return x_pole, y_pole
+        pole_length = 2.0 * self.l
+
+        x_tip = self.x + pole_length * np.sin(self.theta)
+        y_tip = pole_length * np.cos(self.theta)
+
+        return x_tip, y_tip
+
     
-    def angleposition(self):
-        """Return a point representing the angle from vertical.
-        Y > 0 for upwards, X displacement proportional to sin(theta) at fixed radius.
+    def angle_indicator_position(self, radius=1.0):
         """
-        radius = 1.0  # arbitrary length for visualization
-        x_angle = self.x + radius * np.sin(self.theta)
-        y_angle = radius * np.cos(self.theta)
-        return x_angle, y_angle
+        Returns a point used to visualize the pole angle.
+
+        This is NOT a physical point on the pole.
+        It is intended only for plotting the angle direction.
+
+        Parameters
+        ----------
+        radius : float
+            Length of the indicator vector.
+        """
+        x = self.x + radius * np.sin(self.theta)
+        y = radius * np.cos(self.theta)
+
+        return x, y
+
                 
 
 
