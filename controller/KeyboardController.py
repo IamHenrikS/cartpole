42b2a844c0577a42dd7bdcf359e412111ae7b820
IamHenrikS <138816201+IamHenrikS@users.noreply.github.com>
2025-12-25 13:00:04 +0100

    Updated to the function worked

--- controller/KeyboardController.py
+++ controller/KeyboardController.py
@@ -1,15 +1,16 @@
 import pygame
 
 class KeyboardController:
-    def __init__(self, force_mag=10.0):
+    def __init__(self, env, force_mag=10.0):
         self.force_mag = force_mag
+        self.env = env
 
-    def get_force(self):
+    def get_force(self, state, dt):
         keys = pygame.key.get_pressed()
-        force = 0
+        force = 0.0
+
         if keys[pygame.K_LEFT]:
             force = -self.force_mag
         if keys[pygame.K_RIGHT]:
             force = self.force_mag
         return force
-
