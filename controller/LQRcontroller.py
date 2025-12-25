84f2b9412028ee293f1728e603a005c086838859
IamHenrikS <138816201+IamHenrikS@users.noreply.github.com>
2025-12-25 15:02:00 +0100

    remodeled the status to become a saturation

--- controller/LQRcontroller.py
+++ controller/LQRcontroller.py
@@ -8,14 +8,19 @@ https://blog.sackarias.se/controlling-the-cart-pole-using-lqr
 Source 2: Good youtube video of the solution
 https://www.youtube.com/watch?v=hAI8Ag3bzeE
 
+Sources 3: Diagrams of LQR and PID
+https://iopscience.iop.org/article/10.1088/1757-899X/263/5/052007/pdf
+
 # Thoughts for improvement:
 1. Increase the damping of the system for theta, theta_dot in order to
 damp the arm motion for 
+2. 
 
 """
 import numpy as np
 from scipy.signal import cont2discrete     
 from scipy.linalg import solve_discrete_are, solve_continuous_are
+from scipy.signal import StateSpace, TransferFunction
 
 class LQRcontroller:
     def __init__(self, env):
@@ -49,8 +54,9 @@ class LQRcontroller:
                             [-1/(m_t * l * alpha)]
                             ])
 
-        # Defining the solution of the penalizes.
-        self.Q = np.diag([0.0, 0, 3e3, 300])
+        # LQR: 
+        # Disregarding states: x, x_dot due to inheret drift
+        self.Q = np.diag([10e-6, 10e-6, 3e3, 300])
         self.R = np.array([[0.1]])
 
         # The system is remodeled from continous to discrete
@@ -70,6 +76,10 @@ class LQRcontroller:
         poles_d = np.linalg.eigvals(A_cl_d)
         print("Discrete-time closed-loop poles:", poles_d)
 
+        # Note:
+        # The linear systems transfer function can be written in the S-domain
+        # G(s) = C(sI-A)^-1 B + D
+
     def get_force(self, state, dt=None):
         """
         Description: Returns the force as we know
@@ -85,13 +95,13 @@ class LQRcontroller:
         #kd ≈ 0.1 – 0.4
         # This was the final solution we reduce the solution to not care about the system
         # regading x in the LQR and solely handles it with the outside loop with adjusted values
-        # This stabilized the solution at the expected place. Vinst
+        # This stabilized the solution at the expected place.
+        # Implemented as linear state feedback (PD-controller)
+        # Remodeled for saturation
         kx=0.3
         kd=0.25
-        if abs(theta) < np.deg2rad(1):
-            theta_ref = kx * (self.x_ref - x) - kd * x_dot
-        else:
-            theta_ref = 0.0
+        theta_ref = kx * (self.x_ref - x) - kd * x_dot
+        theta_ref = np.clip(theta_ref, -np.deg2rad(5), np.deg2rad(5))
 
         # --- Inner loop (fast LQR) ---
         x_lin = np.array([
@@ -103,12 +113,4 @@ class LQRcontroller:
 
         force = -self.Kd @ x_lin
 
-        """
-        x_lin = np.array([x - self.x_ref, 
-                          x_dot, 
-                          theta - self.theta_ref, 
-                          theta_dot]) 
-        
-        force = - self.Kd @ x_lin
-        """
         return float(force)
