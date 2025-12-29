205b595fc0bc47eac437306baf3dc909a6d5ad4d
IamHenrikS <138816201+IamHenrikS@users.noreply.github.com>
2025-12-29 13:01:09 +0100

    start of ML try using pytorch

---
+++ controller/ML.py
@@ -0,0 +1,13 @@
+"""
+Description: The controller here should utilize the implementation
+of the swingup mechanism and most likely the following ideas are 
+going to be explored:
+
+1. ML solution
+2. Pytorch: https://github.com/microsoft/AI-For-Beginners/blob/main/lessons/6-Other/22-DeepRL/CartPole-RL-TF.ipynb
+"""
+
+class MLcontroller:
+    def __init__(self, env):
+        env = self.env
+        
